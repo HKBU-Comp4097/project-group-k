7be9a4ef4466ad2ea4c16a1ab93f140b82e5b194
hkbu-15200183 <15200183@life.hkbu.edu.hk>
2016-11-02 18:56:26 +0800

    added gps

--- HKBUMapNavigated/app/src/main/java/hk/edu/hkbu/comp/hkbumapnavigated/GmapFragment.java
+++ HKBUMapNavigated/app/src/main/java/hk/edu/hkbu/comp/hkbumapnavigated/GmapFragment.java
@@ -1,12 +1,18 @@
 package hk.edu.hkbu.comp.hkbumapnavigated;
 
+import android.Manifest;
 import android.app.Fragment;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -16,11 +22,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class GmapFragment extends Fragment implements OnMapReadyCallback {
 
+    GoogleMap mMap;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_gmap, container, false);
+
     }
 
     @Override
@@ -34,8 +42,43 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
+        mMap = googleMap;
         LatLng marker = new LatLng(22.338036, 114.181979);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 18));
-        googleMap.addMarker(new MarkerOptions().title("Hello Google Maps!").position(marker));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 18));
+        mMap.addMarker(new MarkerOptions().title("Hello Google Maps!").position(marker));
+        if(checkPermission()) {
+            mMap.setMyLocationEnabled(true);
+        }
+        else{
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+            if(checkPermission()) {
+                mMap.setMyLocationEnabled(true);
+            }
+        }
+        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+
+                    @Override
+                    public void onMyLocationChange(Location arg0) {
+                        // TODO Auto-generated method stub
+                        System.out.println("get");
+
+                        CameraUpdate center= CameraUpdateFactory.newLatLng(new LatLng(arg0.getLatitude(), arg0.getLongitude()));
+                        CameraUpdate zoom=CameraUpdateFactory.zoomTo(12);
+
+
+                        mMap.moveCamera(center);
+                        mMap.animateCamera(zoom);
+                    }
+                });
+
     }
+    private boolean checkPermission() {
+        // Ask for permission if it wasn't granted yet
+        return (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED );
+    }
+
+
 }
