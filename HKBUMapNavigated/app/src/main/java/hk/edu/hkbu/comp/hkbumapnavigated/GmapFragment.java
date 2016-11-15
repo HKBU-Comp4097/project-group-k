46b5807c2d3762ee2450c975c68aecaa4ffe6c86
hkbu-15200183 <15200183@life.hkbu.edu.hk>
2016-11-15 13:43:44 +0800

    merged that something didnt send a pull request

--- HKBUMapNavigated/app/src/main/java/hk/edu/hkbu/comp/hkbumapnavigated/GmapFragment.java
+++ HKBUMapNavigated/app/src/main/java/hk/edu/hkbu/comp/hkbumapnavigated/GmapFragment.java
@@ -104,7 +104,13 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
                         mMap.addMarker(new MarkerOptions().title(locations[i].getName()).position(destinationMarker));
                     }
                 }
-                if(sourceMarker!= null && destinationMarker!=null){
+                if(sourceMarker!=null && destinationMarker==null){
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sourceMarker, 18));
+                }
+                else if(sourceMarker==null && destinationMarker!=null){
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(destinationMarker, 18));
+                }
+                else if(sourceMarker!= null && destinationMarker!=null){
 
                     drawPath(sourceMarker,destinationMarker);
 
@@ -116,6 +122,7 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
                     CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
                     mMap.animateCamera(cu);
                 }
+                else  mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, 18));
             }
         });
 
